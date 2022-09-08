86864b43c3743811c7156c0051abd2f50565a374
jserumaga <j.serumaga@gmail.com>
2022-09-08 12:13:32 -0700

    A C program that prints using the printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.sh>
+
+/**
+ *
+ * Main - Entry point
+ *
+ * Declaration: C program that prints exactly using printf
+ *
+ * Return: return 0
+ *
+ */
+int main(void)
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+
+}
