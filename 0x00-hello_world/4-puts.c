b7d43ae5cb6922aace5ffbc1eb8700e773d2ee85
jserumaga <j.serumaga@gmail.com>
2022-09-09 04:56:16 -0700

    A C program that prints exactly with proper grammar, but the outcome is a piece of art,

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -12,7 +12,7 @@
 int main(void)
 {
 
-	put("\"Programming is like building a multilingual puzzle");
-	return (0);
+	put("Programming is like building a multilingual puzzle,\n");
+	return 0;
 
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,18 +1,13 @@
 #include <stdio.sh>
 
 /**
- *
  * Main - Entry point
  *
- * Declaration: C program that prints exactly using printf
- *
- * Return: return 0
- *
+ * Return: return (0)
  */
 int main(void)
 {
 
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-
 }
