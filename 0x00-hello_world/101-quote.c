5c18fba64d08cc78a6cf4eeb95d4b63bdba92219
jserumaga <j.serumaga@gmail.com>
2022-09-11 11:27:03 -0700

    A C program that prints exactly

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - this is the entry point
+ *
+ * Description:
+ *
+ * Return: Zero when successful
+ *
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+
+	return (0);
+
+}
