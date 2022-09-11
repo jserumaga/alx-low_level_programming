9ffb6d4bc1873e5f5514f7e463ea8cd1e184e03b
jserumaga <j.serumaga@gmail.com>
2022-09-11 10:31:31 -0700

    A program that prints exactly

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,12 +8,11 @@
  *
  * Return: Value 0
  */
- 
 int main(void)
 {
-	char *s = "Programming is like building a multilingual puzzle";
+	char *s = ""Programming is like building a multilingual puzzle";
 
 	puts(s);
-	return 0;
+	return (0);
 
 }
