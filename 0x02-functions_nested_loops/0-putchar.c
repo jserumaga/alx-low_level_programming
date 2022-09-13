57e8a1ed95c7ff706ad8c0d8397d93c803cf95d2
jserumaga <j.serumaga@gmail.com>
2022-09-13 05:00:48 -0700

    A a program that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * main - This is the entry point
+ *
+ * Description:
+ *
+ * Return: return zero when successfull
+ *
+ */
+int main(void)
+{
+	_putchar;
+
+	_putchar('\n');
+	return (0);
+}
