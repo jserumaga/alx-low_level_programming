1e616f27c14ad90cc4c7a1432d81867799af98a5
jserumaga <j.serumaga@gmail.com>
2022-09-13 11:08:14 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,9 +2,13 @@
 #include <stdio.h>
 /**
  * _abs - if the function to print absolute
- * * @n: is the int to be used in the argument
- * * Return: Zero when success
- * */
+ *
+ * @n: is the int to be used in the argument
+ * 
+ * Return: Zero when success
+ *
+ */
+
 int _abs(int n)
 {
 	if (n > 0 || n == 0)
