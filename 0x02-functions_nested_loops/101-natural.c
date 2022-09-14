8ee52c23e87a4dd6f640c455e6063e5422c28da5
jserumaga <j.serumaga@gmail.com>
2022-09-14 07:43:45 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Lists all natural numbers
+ *
+ * Return: Zero
+ *
+ */
+int main(void)
+{
+	int num, sum;
+
+	sum = 0;
+
+	for (num = 0; num < 1024; num++)
+	{
+		if (num % 3 == 0 || num % 5 ==)
+		{
+			sum += num;
+		}
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+
+}
