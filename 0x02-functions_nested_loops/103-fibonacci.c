97cec5c175dd68fd4928bb07eba18996c2e8fb24
jserumaga <j.serumaga@gmail.com>
2022-09-14 07:59:53 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Program entry
+ *
+ * Return: zero
+ *
+ */
+int main(void)
+{
+	long int total_sum, sum, first, second;
+
+	total_sum = 0;
+	sum = 0;
+	first = 0;
+	second = 1;
+
+	while (sum < 4000000)
+	{
+		sum = first + second;
+		if (sum % 2 == 0)
+		{
+			total_sum += sum;
+		}
+		first = second;
+		second = sum;
+	}
+	printf("%li\n", total_sum);
+
+	return (0);
+}
