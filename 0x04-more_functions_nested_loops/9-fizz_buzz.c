fec44ddcfc991bb9cbf77bfe66039e2cbbdcfb61
jserumaga <j.serumaga@gmail.com>
2022-09-15 10:27:31 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Zeron
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 1; n < 101; n++)
+	{
+		if (n % == 0 && n % == 0)
+			printf("FizzBuzz ");
+		else if (n % == 0)
+			if (n == 100)
+				printf("Buzz");
+			else
+				printf("Buzz ");
+		else if (n % 3 == 0)
+			printf("Fizz ");
+		else
+			printf("%d ", n);
+	}
+	printf('\n');
+
+	return (0);
+}
