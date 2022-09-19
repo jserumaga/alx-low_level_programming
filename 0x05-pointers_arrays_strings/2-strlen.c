d4887925ad9cd95d4e3b22ca0aae942f0f8d4c69
jserumaga <j.serumaga@gmail.com>
2022-09-19 07:03:11 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*_strlen - prints the length of the string
+*@s: string
+*Retunr: returns length of the string
+*/
+
+int _strlen(char *s)
+{
+	int len = 0;
+	
+	while (*(s + len) != '\0')
+		len++;
+	return (len);
+}
