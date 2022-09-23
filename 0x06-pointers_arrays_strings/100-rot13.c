836d1c5719c1a1f05fa2d2d6da6bc7be54a79372
jserumaga <j.serumaga@gmail.com>
2022-09-23 08:33:57 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,8 +9,18 @@
 char *rot13(char *str)
 {
 	int indx1 = 0, indx2;
-	char alphabet[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char rot13key[52] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char alphabet[52] = {'a','b','c','d','e','f',
+		'g','h','i','j','k','l','m','n','o','p',
+		'q','r','s','t','u','v','w','x','y','z','A',
+		'B','C','D','E','F','G','H','I','J','K','L',
+		'M','N','O','P','Q','R','S','T','U','V','W',
+		'X','Y','Z'}
+	char rot13key[52] = {'a','b','c','d','e','f',                                                                       
+		'g','h','i','j','k','l','m','n','o','p',                                                      
+		'q','r','s','t','u','v','w','x','y','z','A',                                       
+		'B','C','D','E','F','G','H','I','J','K','L',
+		'M','N','O','P','Q','R','S','T','U','V','W',
+		'X','Y','Z'};
 
 	while (str[indx1])
 	{
