3172b624986876c97f3ced3c33cce502302a7cd6
jserumaga <j.serumaga@gmail.com>
2022-09-26 06:40:01 -0700

    main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -14,7 +14,7 @@
  * *int protoypes (int)
  *
  * */
-int _putchar(char);
+int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -24,3 +24,5 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
+
+#endif /*MAIN_H*/
