52e6661a4bf26b00acc76ed28b5c8b011b39f766
jserumaga <j.serumaga@gmail.com>
2022-09-30 06:42:11 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point for the program
+ * @argc: count of args present
+ * @argv: array of char * pointing to args
+ *
+ * Return: Zero
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	for (i = 1; i < arg; i++)
+	{
+		if (!atoi(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
