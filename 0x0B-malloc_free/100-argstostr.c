26a22a1e07ea9512ca8d49cb6fe15c0add844831
jserumaga <j.serumaga@gmail.com>
2022-10-04 07:44:12 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -14,7 +14,7 @@ char *argstostr(int ac, char **av)
 	char *str;
 	int count = 0, a = 0, b = 0, c = 0;
 
-	if (ac == || av == NULL)
+	if (ac == 0 || av == NULL)
 		return (NULL);
 	while (a < ac)
 	{
