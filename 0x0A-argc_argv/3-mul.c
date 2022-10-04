87f5c551743ad942b4f69c9b5d50d463a41758a0
jserumaga <j.serumaga@gmail.com>
2022-10-04 09:19:32 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - prints the multiplication of two integers
@@ -10,17 +12,15 @@
 
 int main(int argc, char *argv[])
 {
-	int result = 0,num1, num2;
+	int a, b;
 
-	if (argc != 3)
+	if (argc == 1)
 	{
-		printf("%s\n", "Error");
-		return (1);
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		printf("%d\n", a, *b);
+		return (0);
 	}
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-	result = num1 * num2;
-
-	printf("%d\n", result);
-	return (0);
+	printf("Error\n");
+	return (1);
 }
