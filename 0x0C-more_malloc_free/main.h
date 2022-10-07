5c537929d4929eb71b508d358b3d959aa376ae58
jserumaga <j.serumaga@gmail.com>
2022-10-07 07:52:28 -0700

    main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -10,5 +10,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int find_len(char *str);
+int main(int argc, char *argv[]);
+int get_digit(char c);
+char *create_xarray(int size);
+char *iterates_zeroes(char *str);
+void get_prod(char *prod, char *mult, int digit, int zeroes);
+void add_nums(char *final_prod, char *next_prod, int next_len);
 
 #endif /*MAIN_H*/
