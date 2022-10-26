6175571ad4cbffd6dfc7887cd418161c60e395ac
jserumaga <j.serumaga@gmail.com>
2022-10-26 06:58:21 -0700

    3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,5 +1,40 @@
 #include "lists.h"
 
+size_t looped_listint_count(listint_t *head);
+size_t free_listin_safe(listint_t **h);
+
+/**
+ * looped_listint_count - Counts the number of unuque nodes
+ *
+ * @head: A pointer to the head of the listint_t linked list.
+ *
+ * Return: if the list is not looped - 0.
+ */
+
+size_t looped_listint_count(listint_t *head)
+{
+	listint_t *tortoise, *hare;
+	size_t nodes = 1;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+
+	tortoise = head->next;
+	hare = (head->next)->next;
+
+	while (hare)
+	{
+		if (tortoise == hare)
+		{
+			tortoise = head;
+			while (tortoise != hare)
+			{
+				nodes++;
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+
+			tortoise = tortoise->next;
 /**
  * free_listint_safe - Fees a listint_t list safely.
  * @h: A pointer to the address of the head
